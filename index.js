9e6544c6da3163bd81c365427d7cb8898c83ae2b
RedPandaSmash <redpandasmash@gmail.com>
2025-06-30 13:16:23 -0400

    it works

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/2", async (req, res) => {
+  res.sendFile(__dirname + "/2.html");
+});
+
+app.get("/3", async (req, res) => {
+  res.sendFile(__dirname + "/3.html");
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
 });
